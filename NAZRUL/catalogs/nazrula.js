159600830a6164dd2b7c952502645b5bc1dfc08a
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-05-25 16:42:27 +0600

    Add files via upload

---
+++ NAZRUL/catalogs/nazrula.js
@@ -0,0 +1,35 @@
+console.clear();
+const { spawn } = require("child_process");
+const express = require("express");
+const app = express();
+const chalk = require('chalk');
+const logger = require("./nazrulc.js");
+const path = require('path');
+const PORT = process.env.PORT || 8080 || 9000 || 5555 || 5050 || 5000 || 3003 || 2000 || 1029 || 1010;
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/website/nazrul.html'));
+});
+console.clear();
+function startBot(message) {
+    (message) ? logger(message, "starting") : "";
+  console.log(chalk.blue('• X4 NAZRUL X4 SYSTEM •'));
+  logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
+  app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "nazrulb.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
+    });
+  child.on("close", (codeExit) => {
+        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+            startBot();
+            global.countRestart += 1;
+            return;
+        } else return;
+    });
+
+  child.on("error", function(error) {
+    logger("an error occurred : " + JSON.stringify(error), "error");
+  });
+};
+startBot();
