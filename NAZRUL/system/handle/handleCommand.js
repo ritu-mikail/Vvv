30c77d813c619d3e32014f33609b2888c59ee357
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-05-25 20:07:25 +0600

    Update handleCommand.js

--- NAZRUL/system/handle/handleCommand.js
+++ NAZRUL/system/handle/handleCommand.js
@@ -1,7 +1,7 @@
 module.exports = function({ api, models, Users, Threads, Currencies }) {
   const stringSimilarity = require('string-similarity'),
     escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
-    logger = require("../../catalogs/alvic.js");
+    logger = require("../../catalogs/nazrulc.js");
   const axios = require('axios')
   const moment = require("moment-timezone");
   return async function({ event }) {
