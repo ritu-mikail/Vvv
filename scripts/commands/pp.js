9f266cdd2a0e7c8c40ab1ef8e840c8c4267c56db
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-07-16 16:21:11 +0600

    Update and rename Pp.js to pp.js

--- scripts/commands/pp.js
+++ scripts/commands/pp.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "pp",
 	version: "1.0.0",
 	permission: 0,
-	credits: "Nayan",
+	credits: "Nazrul",
 	prefix: false,
 	description: "get user id.",
 	category: "without prefix",
