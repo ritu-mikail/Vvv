fc10d92f04f246b934b12c0bc44c38083c9c09f4
HACKER-NAZRUL-420 <riyamaya7162@gmail.com>
2024-07-16 15:14:31 +0600

    Add files via upload

---
+++ scripts/commands/goiadmin3.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "goiadmin3",
+    version: "1.0.0",
+    permission: 0,
+    credits: "nayan",
+    description: "mention",
+    prefix: true,
+    category: "user",
+    usages: "tag",
+    cooldowns: 5,
+};
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "61560186343783") {
+    var aid = ["61560186343783"];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["আমার বস চিপায়  বিজি আছে\n যা বলার আমাকে বলো", "মেয়ে পটাতে গেছে😁😁😁", "এমন ভাবে মেনশান না দিয়ে একটা জি এফ দাও🙈🙈", "এত ডাকিস কেন😡😡😡\n আমার বস অনেক বিজি", "বস কই তুমি\nতোমারে এক বলদে খোজ করে 😑😑😁🤣"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+    }
+    }}
+};
+module.exports.run = async function({}) {
+                 }
