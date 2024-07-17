f79202fddedb91bb2b58a73375397d5b74c2e26c
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-07-17 10:02:11 +0600

    Update out.js

--- scripts/commands/out.js
+++ scripts/commands/out.js
@@ -1,18 +1,18 @@
-module.exports = {
-  config: {
+module.exports.config = {
     name: "out",
-  version: "1.0.5",
-  credits: "nazrul",
-  prefix: false,
-  permission: 2,
-  description: "out bot",
-  category: "admin",
-  cooldowns: 5
-},
-
-start: async function({ nayan, events, args }) {
-        if (!args[0]) return nayan.removeUserFromGroup(nayan.getCurrentUserID(), events.threadID);
-  nayan.reply("gd bye", events.threadID)
-        if (!isNaN(args[0])) return nayan.removeUserFromGroup(nayan.getCurrentUserID(), args.join(" "));
-}
-}
+    version: "1.0.0",
+    permssion: 2,
+    premium: false,
+    prefix: true,
+    credits: "nazrul",
+    description: "Leave the group",
+    category: "Admin",
+    usages: "out [id]",
+    cooldowns: 10,
+};
+ 
+module.exports.run = async function({ api, event, args }) {
+        if (!args[0]) return api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
+        if (!isNaN(args[0])) return api.removeUserFromGroup(api.getCurrentUserID(), args.join(" "));
+          }
+ 
