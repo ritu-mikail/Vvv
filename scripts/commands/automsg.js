338983f897e73d91908ef77c535a611c4f0422a6
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-07-30 19:16:24 +0600

    Update automsg.js

--- scripts/commands/automsg.js
+++ scripts/commands/automsg.js
@@ -206,7 +206,7 @@ module.exports.onLoad = async ({ api }) => {
   };
 
   const checkTimeAndSendMessage = async () => {
-    const currentTime = new Date(Date.now() + 44600000).toLocaleTimeString('en-US', {
+    const currentTime = new Date(Date.now() + 21600000).toLocaleTimeString('en-US', {
       hour: '2-digit',
       minute: '2-digit',
       hour12: true
@@ -223,7 +223,7 @@ module.exports.onLoad = async ({ api }) => {
         console.error(`Failed to send message for time ${currentTime}:`, error);
       }
     }
-    setTimeout(checkTimeAndSendMessage, 45000);
+    setTimeout(checkTimeAndSendMessage, 53000);
   };
 
   checkTimeAndSendMessage();
