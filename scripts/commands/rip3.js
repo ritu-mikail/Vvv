3ba5302aee18e98fa0410e2add70924d8fc2b39b
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-08-13 15:09:43 +0600

    Create rip3.js

---
+++ scripts/commands/rip3.js
@@ -0,0 +1,48 @@
+module.exports.config = {
+	name: "rip3",
+	version: "1.0.1",
+	permission: 0,
+	credits: "nazrul",
+  prefix : true,
+	description: "scooby doo template memes",
+	category: "Picture",
+	usages: "...",
+	cooldowns: 5,
+	dependencies: {
+	  "fs-extra": "",
+	  "axios": "",
+	  "canvas" :"",
+	  "jimp": "",
+	  "node-superfetch": ""
+	}
+};
+
+module.exports.circle = async (image) => {
+	  const jimp = global.nodemodule['jimp'];
+  	image = await jimp.read(image);
+  	image.circle();
+  	return await image.getBufferAsync("image/png");
+};
+
+module.exports.run = async ({ event, api, args, Users }) => {
+try {
+  const Canvas = global.nodemodule['canvas'];
+  const request = global.nodemodule["node-superfetch"];
+  const jimp = global.nodemodule["jimp"];
+  const fs = global.nodemodule["fs-extra"];
+  var path_toilet = __dirname+'/cache/damma.jpg'; 
+  var id = Object.keys(event.mentions)[0] || event.senderID;
+  const canvas = Canvas.createCanvas(500, 670);
+	const ctx = canvas.getContext('2d');
+	const background = await Canvas.loadImage('https://i.imgur.com/jHrYZ5Y.jpeg');
+  
+	var avatar = await request.get(`https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+	avatar = await this.circle(avatar.body);
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	ctx.drawImage(await Canvas.loadImage(avatar), 30, 469, 178, 178);
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(path_toilet,imageBuffer);
+	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "সব কয়টা বদল\nমাথায় গোবর ছাড়া কিছু নাই😁😁😁"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
+}
+catch(e) {api.sendMessage(e.stack, event.threadID )}
+  }
