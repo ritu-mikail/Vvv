081a30d80ae5a4b2bd25bd1e14542b09c1c17268
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-08-20 09:56:25 +0600

    Update 4k.js

--- scripts/commands/4k.js
+++ scripts/commands/4k.js
@@ -1,63 +1,51 @@
-module.exports.config = {
-    name: "hd",
-    version: "1.0.0",
+const a = require('axios');
+const tinyurl = require('tinyurl');
+
+module.exports = {
+  config: {
+    name: "4k",
+    version: "1.0",
     permission: 0,
-    credits: "Nayan",
-    description: "",
+    credits: "MR.AYAN",
     prefix: true,
-    category: "prefix",
-    usages: "[model]",
-    cooldowns: 10,
-    dependencies: {
-       'nayan-server': ''
+    countDown: 15,
+    description: "Upscale your image.",
+    category: "image",
+    usages: "reply to an image"
     }
-};
+  },
 
+  module.exports.run = async function ({ message, args, event, api }) {
+    let imageUrl;
 
+    if (event.type === "message_reply") {
+      const replyAttachment = event.messageReply.attachments[0];
 
+      if (["photo", "sticker"].includes(replyAttachment?.type)) {
+        imageUrl = replyAttachment.url;
+      } else {
+        return api.sendMessage(
+          { body: "⚠️ | Reply must be an image." },
+          event.threadID
+        );
+      }
+    } else if (args[0]?.match(/(https?:\/\/.*\.(?:png|jpg|jpeg))/g)) {
+      imageUrl = args[0];
+    } else {
+      return api.sendMessage({ body: "❌ | Reply to an image." }, event.threadID);
+    }
 
+    try {
+      const url = await tinyurl.shorten(imageUrl);
+      const k = await a.get(`https://www.api.vyturex.com/upscale?imageUrl=${url}`);
 
-module.exports.run = async function({ api, event, args }) {
-  
-    const axios = require("axios")
-    const request = require("request")
-    const fs = require("fs-extra")
-    const {upscale} = require('nayan-server')
-          if (event.type !== "message_reply") return api.sendMessage("[❗]➜ You must reply to a photo", event.threadID, event.messageID);
-        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("[❗]➜ You must reply to a photo", event.threadID, event.messageID);
-        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("[❓]➜ This is not an image", event.threadID, event.messageID);
-  const content = (event.type == "message_reply") ? event.messageReply.attachments[0].url : args.join(" ");
-  
-  const mod = args[0];
-  
-const model = mod
-  try {
-const res = await upscale(content, model)
-  console.log(res)
-  api.setMessageReaction("✅", event.messageID, (err) => {
-      }, true);
-  const img1 = res.image_url
-  const job = res.job_id
-        var msg = [];
+      message.reply("☢️ | Wait for some time••");
 
-  const pic = (
-    await axios.get(`https://images.prodia.xyz/${job}.png`,
-      { responseType: 'stream' }
-    )
-  ).data;
+      const resultUrl = k.data.resultUrl;
 
-        {
-            msg += `✅HERE YOUR PHOTO`
-        }
-
-        return api.sendMessage({
-            body: msg,
-            attachment: pic
-
-        }, event.threadID, event.messageID);
-     } catch (err) {
-     api.setMessageReaction("❌", event.messageID, (err) => {
-    }, true);
-      api.sendMessage(`🔰Use ${global.config.PREFIX}${this.config.name} [model]\n🔰Example:${global.config.PREFIX}${this.config.name} 1\n\n🔥Total Model limit 2...`, event.threadID, event.messageID);  
-     }
-  };
+      message.reply({ body: "✨ | Done 4ᴋ ɪᴍᴀɢᴇ\n\nʜᴇʀᴇ ᴛʜɪs ɪs ʏᴏᴜʀ ᴘʜᴏᴛᴏ", attachment: await global.utils.getStreamFromURL(resultUrl) });
+    } catch (error) {
+      message.reply("❌ | Error: " + error.message);
+    }
+  }
+}; 
