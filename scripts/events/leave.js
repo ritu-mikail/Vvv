ec7ddd60494c922d455f8a70353c56aed2aa48ae
ritu-mikail <mdmikail673@gmail.com>
2024-08-23 19:16:48 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "leave",
   eventType: ["log:unsubscribe"],
   version: "1.0.0",
-  credits: "Mirai Team",
+  credits: "nazrul",
   description: "Thông báo bot hoặc người rời khỏi nhóm",
   dependencies: {
     "fs-extra": "",
@@ -17,14 +17,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
   const { threadID } = event;
   const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
   const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  এখানে জিঁলাপিঁ থাকতে তুই লিভ নিবি 😺😸😹🤣😹 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----";
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "  তোর সাহস কম নয় এখানে নজরুল এর ইসলামিক চ্যাট থাকতে তুই লিভ নিস😡😠🤬 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----" : "তোমার এই গ্রুপে থাকার কোনো যোগ্যাতা নেই আবাল😡।\nতাই তোমার লাথি মেরে গ্রুপ থেকে বের করে দেওয়া হলো🤪। WELLCOME REMOVE🤧 \n✢━━━━━━━━━━━━━━━✢\n ----❖----- 𝐍𝐀𝐙𝐑𝐔𝐋 -----❖----";
   const path = join(__dirname, "Nazrul", "leaveGif");
-  const gifPath = join(path, `out.jpeg`);
+  const gifPath = join(path, `leave1.gif`);
   var msg, formPush
 
   if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-  (typeof data.customLeave == "undefined") ? msg = "হায়রে কপাল {name} {type} " : msg = data.customLeave;
+  (typeof data.customLeave == "undefined") ? msg = "ইস {name} {type} " : msg = data.customLeave;
   msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
   if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
