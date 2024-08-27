4bc5d64a8754cb88c8ce9201d405949f5e3c94a1
ritu-mikail <mdmikail673@gmail.com>
2024-08-27 11:55:50 +0600

    Create jui.js

---
+++ scripts/commands/jui.js
@@ -0,0 +1,27 @@
+
+/**
+* @Shaon Ahmed 
+* @warn Do not edit code or edit credits
+* @Dont Change This Credits Otherwisw Your Bot Lol
+*/
+module.exports.config = {
+  name: "jui",
+  version: "11.9.7",
+  permssion: 0,
+  credits: "Shaon Ahmed",
+  prefix:false,
+  description: "SIM",
+  category: "Smi",
+  usages: "janu",
+  cooldowns: 30,
+};
+ 
+module.exports.run = async ({ api, event,args }) => {
+const axios = require("axios");
+let query = args.join(" ");
+if (!query)
+    return api.sendMessage(`হুম জান বলো কি বলবা-!!❤️✌️`, event.threadID, event.messageID);
+const res = await axios.get(`https://nazrul-all.onrender.com/sim?type=ask&ask=${query}`);
+var plaintext = res.data.answer;
+api.sendMessage(plaintext, event.threadID, event.messageID)
+}
